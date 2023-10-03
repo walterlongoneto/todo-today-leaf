f24cf98b9caf5d8bff914d88533df7825d8319ed
Walter Longo Neto <walterlongoneto@gmail.com>
2023-10-03 11:39:43 -0300

    Initializing app models~

--- src/components/models.ts
+++ src/components/models.ts
@@ -6,3 +6,23 @@ export interface Todo {
 export interface Meta {
   totalCount: number;
 }
+
+export interface Day {
+  startedAt: Date | null;
+  finishedAt: Date | null;
+  tasks: Array<Task>;
+}
+
+export interface Task {
+  description: string;
+  status: string;
+  totalPomodoros: number;
+  pomodoros: Array<Pomodoro>;
+  updatedAt?: Date;
+}
+
+export interface Pomodoro {
+  secondPlanning: boolean;
+  status: string;
+  updatedAt: Date | null;
+}
